6211a43fc518c5f508ee489a33cde226624ea2a4
vaibhavkhamitkar12 <48472833+vaibhavkhamitkar12@users.noreply.github.com>
2024-03-02 18:24:05 +0530

    Create streamlit_check.py

---
+++ streamlit_check.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pyodbc
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + st.secrets["server"]
+        + ";DATABASE="
+        + st.secrets["database"]
+    )
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from Matches;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
