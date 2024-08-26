dcd1a72f8c02c35d681c94bd57b7db0e74acecc3
vaibhavkhamitkar12 <48472833+vaibhavkhamitkar12@users.noreply.github.com>
2024-08-26 14:44:27 +0530

    Update app.py

--- CricAI/app.py
+++ CricAI/app.py
@@ -28,57 +28,8 @@ final_prompt = ChatPromptTemplate.from_messages(
     [
         ("system", 
          """
-          You are a helpful AI assistant expert in querying SQL Database to find answers to user's question about Matches(
-          ID: A unique identifier for each cricket match, column type - int
-City: The city where the match took place, providing geographical context for the event. column type - varchar
-Date: The date of the match.
-Season: The season year of that match.
-MatchNumber: The match number within the season and also describes type of matches like Final,
-Qualifier 2,
-Eliminator,
-Qualifier 1,if match is of playoffs type.
-Team1: The name of the first team playing in the match. column type - varchar
-Team2: The name of the second team playing in the match. column type - varchar
-Venue: The name of the stadium where the match was held. column type - varchar
-TossWinner: The name of team winning the toss. column type - varchar
-TossDecision: The decision made by the toss-winning team which can be bat or field. column type - varchar
-SuperOver: Whether a Super Over was played to break a tie, Y if played , N if super over is not player. 
-WinningTeam: The name of team that won the that match. column type - varchar
-WonBy: Describes how the match was won by Runs,Wickets, SuperOver or NoResult. column type - varchar
-Margin: The margin of victory, if corresponding WonBy cell is "Runs" then no. of runs would mentioned, if corresponding Wonby cell is "Wickets" then no. of wickets by which winning team won would be monetioned. column type - int
-Method: The method of winning either D/L or NA , if there is rain then D/L method is used , otherwise NA which describes normal match. column type - varchar
-Player_of_Match: The name of Man of the Match player. column type - varchar 
-
-Team1Players: A list of players in Team 1, providing information about the team composition. Example - ["Player1","Player2","Player3","Player4","Player5","Player6","Player7","Player8","Player9","Player10","Player11"]
-
-Team2Players: A list of players in Team 2, similar example as Team1Players column
-
-Umpire1: The name of first umpire officiating the match. column type - varchar
-
-Umpire2: The name of second umpire officiating the match. column type - varchar)
-and ball_to_ball 
-(
-ID: Unique identifier for each cricket match.
-innings: Indicates the inning number of the match (1 or 2).
-overs: The current over of the match.In range of 0 to 20
-ballnumber: The sequential number of the ball within the over. 6 balls in an over
-batter: The name of batsman currently facing the ball. 
-bowler: The name bowler delivering the ball.
-non-striker: The name of non-striker batsman at the crease.
-extra_type: Type of extra run scored in the ball (e.g., legbyes, wides, byes,No ball, penalty,N/A - if no extra run is scored in that ball).
-batsman_run: Number of runs scored by the batsman in that particular ball of that particular over.
-extras_run: Additional runs scored through extras if any in that particular ball of that particular over.
-total_run: Total runs scored in the ball, including extras.
-non_boundary: Indicates if the run was a boundary (if non boundary then 1, if boundary or 6 then value is 1).
-isWicketDelivery: Indicates if the ball resulted in a wicket.(If wicket delivery then value is 1 , if not a wicket delivery than value is 0)
-player_out: Name of the Batsman dismissed (if applicable, otherwise N/A).
-kind: Type of dismissal (e.g., caught, bowled, caught and bowled, hit wicket, stumped,lbw, run out,obstructing the field, retired hurt, ).
-fielders_involved: Name of the Fielders involved in the dismissal.(only if caught, caught and bowled, stumped, run out in corresponding kind of dismissal field)
-BattingTeam: The Name of the team batting during this phase of the match.
-)
-
-Note : ID is the primary key for matches and ball_by_ball
-
+          You are a helpful AI assistant expert in querying SQL Database.
+          You are an assistant that provides statistical and analytical answers to user's question about Matches and ball_to_ball table.         
          """
          ),
         ("user", "{question}\n ai: "),
